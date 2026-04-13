1becbec1edcc371136cb871f8ae3fc5547a45d78
priyalgit config --global user.email <priyagit config --global user.name>
2026-04-13 20:10:16 +0530

    what you changed

---
+++ priyadharshini.py
@@ -0,0 +1,17 @@
+print("I like biryani")
+print("I like sleeping")
+print("I like egg and chicken")
+
+#variables
+
+full_name="priyadharshini"
+age=18
+gpa=9.71
+is_student=True
+print(f"hellow{full_name}")
+print(f"you are{age}years old")
+print(f"your gpa is {gpa}")
+if is_student:
+    print("you are a student")
+else:
+    print("you are NOT a student")    
